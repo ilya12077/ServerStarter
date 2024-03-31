2221aa03026e9340d593872689a3aa90dd9598fa
ilya12077 <mr.ilya.1207@gmail.com>
2024-03-31 21:18:24 +0300

    fix /list

--- telegrambot/src/main.py
+++ telegrambot/src/main.py
@@ -281,7 +281,7 @@ def main_handler(r):
                                 with open(f'{path}names.json', 'w') as f:
                                     json.dump(ids, f, indent=2)
                             else:
-                                send_message(user_id, f'Сейчас на сервере {player} игроков: <i>{list_players}</i>, не могу выключить. Попросите администратора выключить сервер', keyboards(user_id))
+                                send_message(user_id, f'Сейчас на сервере {players} игроков: <i>{list_players}</i>, не могу выключить. Попросите администратора выключить сервер', keyboards(user_id))
                         else:
                             status_code = close_server(user_id, timeout=5)
                             if status_code == 200:
